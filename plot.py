6a193a10f4a3cff07c6e227cf2240f2652e0c389
LEBREUILLY Ugo <ugo.lebreuilly@cea.fr>
2023-02-03 11:39:21 +0100

    Backbone of the library

--- plot.py
+++ plot.py
@@ -6,8 +6,8 @@ from matplotlib.ticker import MaxNLocator
 from matplotlib.colors import ListedColormap
 from matplotlib.ticker import MaxNLocator
 
-def plot_slice(sharkdata,var,scalar_args={"cmap":'viridis','levels':200},cbar_args=None):
-	plt.contourf(sharkdata.xx,sharkdata.yy,getattr(sharkdata,var),**scalar_args)
+def plot_slice(sharkdata,var,levels=100,scalar_args={"cmap":'viridis'},cbar_args=None):
+	plt.contourf(sharkdata.xx,sharkdata.yy,getattr(sharkdata,var),levels=levels,**scalar_args)
 	#plt.colorbar(cbar_args)
 	plt.xlabel("x")
 	plt.ylabel("y")
