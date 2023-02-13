30833da7603884c05fcab27552773fab51458312
LEBREUILLY Ugo <ugo.lebreuilly@cea.fr>
2023-02-13 11:02:38 +0100

    cbar

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ from matplotlib.ticker import MaxNLocator
 def plot_slice(sharkdata,var,scalar_args={"cmap":'viridis'},cbar_args=None):
 	im=plt.pcolormesh(sharkdata.xx,sharkdata.yy,getattr(sharkdata,var),**scalar_args)
 	if(cbar_args is not None):
-		plt.colorbar(im,**cbar_args)
+		cb=plt.colorbar(im,**cbar_args)
 	plt.xlabel("x")
 	plt.ylabel("y")
 	plt.legend()
